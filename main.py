edf9b1b7485fd3cbe95edde2f0459ec8a7a541fe
AdamKane <akane@forgefx.com>
2024-08-28 02:14:29 -1000

    Refactor root route to return a more descriptive message

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ slack_api = SlackAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World?"}
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
